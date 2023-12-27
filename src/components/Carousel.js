e0ae514fe1e80ab7c92b5667ceada68bd6ffb8d3
Abdelouahab Bella <abdobella977@gmail.com>
2023-12-27 13:26:22 +0100

    Update Carousel component and styles

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,42 +1,76 @@
-import '../assets/css/carsual.css'
+// import '../assets/css/carsual.css'
+import './index.scss';
 
 export default function Carousel({ carouselImages}) {
-    console.log("carouselImages", carouselImages);
-    return (
-      <div className="carousel">
-        <form className="carousel-form">
-            {carouselImages && carouselImages.length > 0 && carouselImages.map((image, index) => {
-                return (
-                  <div key={index}>
-                    <input
-                      type="radio"
-                      name="fancy"
-                      autoFocus
-                      value="clubs"
-                      id="clubs"
-                    />
-                    <label
-                      className="carousel-label"
-                      htmlFor="clubs"
-                      style={{
-                        background: `url(http://localhost:5000/${image.img})`,
-                      }}
-                    >
-                      &#9827; Clubs
-                    </label>
-                  </div>
-                );
-            })}
-          {/* <input type="radio" name="fancy" autoFocus value="clubs" id="clubs" />
-            <input type="radio" name="fancy" value="hearts" id="hearts" />
-            <input type="radio" name="fancy" value="spades" id="spades" />
-            <input type="radio" name="fancy" value="diamonds" id="diamonds" />			 */}
-          {/* <label className='carousel-label' htmlFor="clubs">&#9827; Clubs</label>
-            <label className='carousel-label' htmlFor="hearts">&#9829; Hearts</label>
-            <label className='carousel-label' htmlFor="spades">&#9824; Spades</label>
-            <label className='carousel-label' htmlFor="diamonds">&#9830; Diamonds</label> */}
-          <div className="keys">Use left and right keys to navigate</div>
-        </form>
+  console.log("carouselImages", carouselImages);
+  const boxes = document.querySelectorAll(".box");
+  let activeIndex = 1;
+  let isTransitioning = false;
+
+  function updateCurrentImg() {
+    isTransitioning = true;
+
+    boxes.forEach((box, index) => {
+      const isActive = index === activeIndex;
+      box.classList.toggle("expanded", isActive);
+      box.classList.toggle("closed", !isActive);
+    });
+
+    setTimeout(() => {
+      isTransitioning = false;
+    }, 500);
+  }
+
+  function handleArrowKey(event) {
+    if (isTransitioning) {
+      return;
+    }
+
+    if (event.key === "ArrowRight") {
+      activeIndex = (activeIndex + 1) % boxes.length;
+    } else if (event.key === "ArrowLeft") {
+      activeIndex = (activeIndex - 1 + boxes.length) % boxes.length;
+    }
+
+    updateCurrentImg();
+  }
+
+  function handleBoxClick(index) {
+    if (isTransitioning) {
+      return;
+    }
+
+    if (index === activeIndex && boxes[index].classList.contains("expanded")) {
+      boxes.forEach((box) => box.classList.remove("closed", "expanded"));
+      activeIndex = 0;
+    } else {
+      activeIndex = index;
+      updateCurrentImg();
+    }
+  }
+
+  document.addEventListener("keydown", handleArrowKey);
+
+  updateCurrentImg();
+
+  boxes.forEach((box, index) => {
+    box.addEventListener("click", () => handleBoxClick(index));
+  });
+
+  return (
+    <div className="box-container">
+      <div className="box">
+        <div className="overlay"></div>
       </div>
-    );
+      <div className="box">
+        <div className="overlay"></div>
+      </div>
+      <div className="box">
+        <div className="overlay"></div>
+      </div>
+      <div className="box">
+        <div className="overlay"></div>
+      </div>
+    </div>
+  );
 }
