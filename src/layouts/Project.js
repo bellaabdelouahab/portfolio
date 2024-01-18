941a20f88f02524231b1fb177f5b770d348d4ff8
Abdelouahab Bella <abdobella977@gmail.com>
2024-01-18 20:13:41 +0100

    add style for projects page and project page

--- src/layouts/Project.js
+++ src/layouts/Project.js
@@ -3,9 +3,24 @@ import CodeSamples from '../components/CodeSamples';
 import Carousel from '../components/Carousel';
 import axios from 'axios';
 import { useLoaderData } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 export default function Project() {
   const project = useLoaderData();
+
+  const dataSources = [
+    "https://img.icons8.com/color/250/000000/microsoft-excel-2019--v1.png",
+    "https://img.icons8.com/color/250/000000/csv.png",
+    "https://img.icons8.com/color/250/000000/json--v1.png",
+    "https://img.icons8.com/color/250/000000/microsoft-sql-server.png",
+    "https://img.icons8.com/color/250/000000/mysql-logo.png",
+    "https://img.icons8.com/color/250/000000/mongodb.png",
+    "https://img.icons8.com/color/250/000000/python.png",
+    "https://img.icons8.com/color/250/000000/xml.png",
+  ];
+
+
   console.log("project", project);
   
   return ( 
@@ -18,12 +33,20 @@ export default function Project() {
           }}
         />
         <div className="project__info">
-          <h1 className="project__info__title">
+          <h1 className="title">
             {project.title}
           </h1>
-          <p className="project__info__description">
+          <p className="description">
             {project.description}
           </p>
+          <div className="bottom_options">
+            <p className="project__dates">
+              13-06-2023 ⇒ 07-09-2023 | Duration : {"6 months"}
+            </p>
+            <a href={project.githubLink} target="_blank" rel="noreferrer">
+              <FontAwesomeIcon icon={faGithub} />
+            </a>
+          </div>
         </div>
       </div>
       {/* tools */}
@@ -53,44 +76,23 @@ export default function Project() {
         </ul>
       </div>
 
-      {/* Data Scources for data scince projects */}
-      {/* this shoulb be different than tools section , it should be cards
-      in each card it should have file type in the center of the card as image taking 80 % of the card with padding 1rem
-      and the file name on the right bottom and on the left bottom file size  */}
       {/* {project.dataSources && */}
          {/* project.dataSources.length > 0 && */}
         {/* ( */}
           <div className="project__data-sources">
             <h2 className="project__data-sources__title">Data Sources</h2>
+            <hr />
             <div className="project__data-sources__list">
             {/* {project.dataSources.map((dataSource, index) => ( */}
-            <div key={"index"} className="project__data-sources__item">
-              <div className="project__data-sources__item__image">
-                <img src="https://img.icons8.com/color/48/000000/microsoft-excel-2019--v1.png" alt="excel" />
-              </div>
-              <div className="project__data-sources__item__info">
-                <p className="project__data-sources__item__info__name">{"dataSource.name"}</p>
-                <p className="project__data-sources__item__info__size">{"dataSource.size"}</p>
-              </div>
-            </div>
-            <div key={"index"} className="project__data-sources__item">
-              <div className="project__data-sources__item__image">
-                <img src="https://img.icons8.com/color/48/000000/microsoft-excel-2019--v1.png" alt="excel" />
-              </div>
-              <div className="project__data-sources__item__info">
-                <p className="project__data-sources__item__info__name">{"dataSource.name"}</p>
-                <p className="project__data-sources__item__info__size">{"dataSource.size"}</p>
-              </div>
-            </div>
-            <div key={"index"} className="project__data-sources__item">
-              <div className="project__data-sources__item__image">
-                <img src="https://img.icons8.com/color/150/000000/microsoft-excel-2019--v1.png" alt="excel" />
-              </div>
-              <div className="project__data-sources__item__info">
-                <p className="project__data-sources__item__info__name">{"dataSource.name"}</p>
-                <p className="project__data-sources__item__info__size">142Mb</p>
-              </div>
-            </div>
+                <div key={"index"} className="project__data-sources__item">
+                  <div className="project__data-sources__item__image">
+                    <img src={dataSources[0]} alt="excel" />
+                  </div>
+                  <div className="project__data-sources__item__info">
+                    <p className="project__data-sources__item__info__name">{"dataSource.name"}</p>
+                    <p className="project__data-sources__item__info__size">142Mb</p>
+                  </div>
+                </div>
               {/* ))} */}
             </div>
           </div>
