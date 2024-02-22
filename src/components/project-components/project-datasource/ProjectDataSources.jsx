ccb1c08fee7876740955b94388849f7ef3800f42
Abdelouahab Bella <abdobella977@gmail.com>
2024-02-22 13:45:22 +0100

    Refactor data source rendering in ProjectDataSources component

--- src/components/project-components/project-datasource/ProjectDataSources.jsx
+++ src/components/project-components/project-datasource/ProjectDataSources.jsx
@@ -13,24 +13,24 @@ export default function ProjectDataSources({dataSources}){
       ];
 
     return (
-        <div className="project__data-sources">
-            <h2 className="project__data-sources__title">Data Sources</h2>
-            <hr />
-            <div className="project__data-sources__list">
-                {dataSources.map((dataSource, index) => (
-                    <div key={"index"} className="project__data-sources__item">
-                        <div className="project__data-sources__item__image">
-                            <img src={dataSourcesImg[index]} alt="excel" />
-                        </div>
-                        <div className="project__data-sources__item__info">
-                        <p className="project__data-sources__item__info__name">
-                            {dataSource.name}
-                        </p>
-                        <p className="project__data-sources__item__info__size">142Mb</p>
-                        </div>
-                    </div>
-                ))}
+      <div className="project__data-sources">
+        <h2 className="project__data-sources__title">Data Sources</h2>
+        <hr />
+        <div className="project__data-sources__list">
+          {dataSources.map((dataSource, index) => (
+            <div key={"index"} className="project__data-sources__item">
+              <div className="project__data-sources__item__image">
+                <img src={dataSourcesImg[dataSource.type]} alt="excel" />
+              </div>
+              <div className="project__data-sources__item__info">
+                <p className="project__data-sources__item__info__name">
+                  {dataSource.name}
+                </p>
+                <p className="project__data-sources__item__info__size">142Mb</p>
+              </div>
             </div>
+          ))}
+        </div>
       </div>
-    )
+    );
 }
