3d33762dcdfaeb8b25c09853e484cc103fc40e3a
Abdelouahab Bella <abdobella977@gmail.com>
2024-03-04 03:04:59 +0100

    Add show more button to Certificates view

--- src/views/Certificates.jsx
+++ src/views/Certificates.jsx
@@ -1,28 +1,65 @@
 import "assets/css/certification.css";
+import { useEffect,useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import axiosInstance from "utils/axios";
 const backendUploadsApi = process.env.BACKEND_UPLOADS_API;
 
 export default function Certificates() {
-    const {certificates,count} = useLoaderData();
+    const {certificatesInit,count} = useLoaderData();
+    const [certificates, setCertificates] = useState(certificatesInit);
+    const [page, setPage] = useState(1);
+
+    // a handler for the show more button
+    useEffect(() => {
+        const handleShowMore = () => {
+            setPage(page + 1);
+            // fetch more certificates
+            axiosInstance.get(`certificates?limit=9&page=${page + 1}`)
+                .then(res => {
+                    const newCertificates = res.data;
+                    console.log(newCertificates);
+                    setCertificates([...certificates, ...newCertificates]);
+
+                })
+                .catch(err => console.log(err));
+        };
+        const showMoreButton = document.querySelector(".certifications-show-more button");
+        showMoreButton.addEventListener("click", handleShowMore);
+        return () => {
+            showMoreButton.removeEventListener("click", handleShowMore);
+        };
+    }, [page]);
+
 
     return (
-        <>
-            <div className="certifications-header">
-                <div className="count">+{count} Badges & Certifications</div>
-                <label>
-                    Sorted By :<span className="filter"> Date Earned ▼</span>{" "}
-                </label>
+      <>
+        <div className="certifications-header">
+          <div className="count">+{count} Badges & Certifications</div>
+          <label>
+            Sorted By :<span className="filter"> Date Earned ▼</span>{" "}
+          </label>
+        </div>
+        <div className="certifications-content">
+          {certificates.map((certificate, index) => (
+            <div className="certification-element" key={index}>
+              <img
+                src={`${backendUploadsApi}${certificate.image}`}
+                alt="NoImage"
+                width="100"
+                height="100"
+              />
+              <div className="certification-title">{certificate.title}</div>
             </div>
-            <div className="certifications-content">
-                {certificates.map((certificate, index) => (
-                    <div className="certification-element" key={index}>
-                        <img src={`${backendUploadsApi}${certificate.image}`} alt="NoImage" width="100" height="100" />
-                        <div className="certification-title">{certificate.title}</div>
-                    </div>
-                ))}
-            </div>
-        </>
+          ))}
+        </div>
+        {/* div for show more */}
+        <div className="certifications-show-more">
+            <div className="line"></div>
+          <button 
+          >Show More</button>
+            <div className="line"></div>
+        </div>
+      </>
     );
 }
 
@@ -30,6 +67,6 @@ export async function getCertificates() {
     const res = await axiosInstance.get("certificates?limit=9");
     const certificates = res.data;
     const count = await axiosInstance.get("certificates/count");
-    const data = { certificates, count: count.data };
+    const data = { certificatesInit: certificates, count: count.data };
     return data;
 }
