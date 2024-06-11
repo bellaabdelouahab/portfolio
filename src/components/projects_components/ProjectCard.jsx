8e9414e4bae445c0c535524fa4396f7e91b7c4f7
Abdelouahab Bella <abdobella977@gmail.com>
2024-06-11 16:41:23 +0100

    add react preloader skeleton

--- src/components/projects_components/ProjectCard.jsx
+++ src/components/projects_components/ProjectCard.jsx
@@ -1,9 +1,22 @@
+import { useState, useEffect } from "react";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
 const backendUploadsApi = process.env.BACKEND_UPLOADS_API;
+
 export function ProjectCard({ project }) {
   const { title, description, image, githubLink, highlighted } = project;
+  const [imageLoaded, setImageLoaded] = useState(false);
+
   let truncatedDescription = description.slice(0, 250);
   const lastSpaceIndex = truncatedDescription.lastIndexOf(" ");
   truncatedDescription = truncatedDescription.slice(0, lastSpaceIndex);
+
+  useEffect(() => {
+    const img = new Image();
+    img.src = `${backendUploadsApi}${image}`;
+    img.onload = () => setImageLoaded(true);
+  }, [image]);
 
   return (
     <div
@@ -15,16 +28,39 @@ export function ProjectCard({ project }) {
       <div
         className="home-projects-section__projects__project__img"
         style={{
-          backgroundImage: `url(${backendUploadsApi}${image})`,
+          backgroundImage: imageLoaded
+            ? `url(${backendUploadsApi}${image})`
+            : "none",
           backgroundSize: "100.5% 100%",
         }}
-      />
-      <h3 className="home-projects-section__projects__project__title">
-        {title}
-      </h3>
-      <p className="home-projects-section__projects__project__description">
-        {truncatedDescription}...
-      </p>
+      >
+        {!imageLoaded && <Skeleton height="100%" />}
+      </div>
+      {imageLoaded ? (
+        <>
+          <h3 className="home-projects-section__projects__project__title">
+            {title}
+          </h3>
+          <p className="home-projects-section__projects__project__description">
+            {truncatedDescription}...
+          </p>
+        </>
+      ) : (
+        <div>
+          <Skeleton
+            height={24}
+            width={`60%`}
+            style={{
+              marginBottom: "10px",
+              marginTop: "10px",
+              marginLeft: "10px",
+            }}
+          />
+          <div className="home-projects-section__projects__project__description">
+            <Skeleton count={7} />
+          </div>
+        </div>
+      )}
       <div className="home-projects-section__projects__project__buttons">
         <a
           className="home-projects-section__projects__project__buttons__view"
