febd2cb1dbd9c49db0c898d49fa8af0f021c5d47
bellaabdelouahab <a.bella4246@uca.ac.ma>
2025-03-13 01:48:42 +0000

    Deploying to gh-pages from @ bellaabdelouahab/portfolio@73e4a9d462faf451b17b8c315ce0c36924c23570 🚀

---
+++ assets/NotFound-8R8URpRv.js
@@ -0,0 +1,1 @@
+import{b as o,j as e}from"./index-oTqBNcYk.js";const n=()=>{const t=o();return e.jsxs("div",{style:{textAlign:"center",color:"white"},children:[e.jsx("h1",{children:"404 - Page Not Found"}),e.jsxs("p",{children:["The requested URL ",e.jsx("code",{children:t.pathname})," was not found on this server."]})]})};export{n as default};
