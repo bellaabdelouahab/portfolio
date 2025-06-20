894a8770c6e45855e137162334d0fa654e180d7f
bellaabdelouahab <a.bella4246@uca.ac.ma>
2025-06-20 16:23:17 +0000

    Deploying to gh-pages from @ bellaabdelouahab/portfolio@712da505b40d765d52d4f20c985a44304ca75dfc 🚀

--- assets/NotFound-DyMG_NIa.js
+++
@@ -1,1 +0,0 @@
-import{b as o,j as e}from"./index-dFN3F_9_.js";const n=()=>{const t=o();return e.jsxs("div",{style:{textAlign:"center",color:"white"},children:[e.jsx("h1",{children:"404 - Page Not Found"}),e.jsxs("p",{children:["The requested URL ",e.jsx("code",{children:t.pathname})," was not found on this server."]})]})};export{n as default};
